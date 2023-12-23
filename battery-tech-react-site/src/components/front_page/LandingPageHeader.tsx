f81ef5ac1899788edcc77023d71d8980866399cb
wjthompso <49210425+wjthompso@users.noreply.github.com>
2023-12-23 16:58:35 -0600

    Changed landing page: Center text and new background-image

--- battery-tech-react-site/src/components/front_page/LandingPageHeader.tsx
+++ battery-tech-react-site/src/components/front_page/LandingPageHeader.tsx
@@ -14,20 +14,21 @@ const LandingPageHeader: React.FC = () => {
   return (
     <div className="landing-page-header">
         <div className="landing-page-header-text">
-          <span style={{ fontFamily: 'Inter, sans-serif', fontWeight: 800, textAlign: "left"}}>
-            CellStatus
-          </span>
-          <span style={{ fontFamily: 'Inter, sans-serif', fontWeight: 600, textAlign: "left"}}>
+          {/* <span style={{ fontFamily: 'Inter, sans-serif', fontWeight: 800, textAlign: "left"}}>
+            CellStatus<br/>
+          </span> */}
+          <span style={{ fontFamily: 'Inter, sans-serif', fontWeight: 600, }}>
             {" "}
           </span>
-          <span style={{ fontFamily: 'Inter, sans-serif', fontWeight: 500, textAlign: "left" }}>
-            The current status of battery tech
-          </span>
-          <div className="div-4">
-          Showing the current status of different battery technologies
-          currently in research and development, categorized by their ion.
-        </div>
-        </div>
+          <span style={{ fontFamily: 'Inter, sans-serif', fontWeight: 500 }}>
+            The current status of battery technology
+        </span>
+      </div>
+      <div className="landing-page-subheader-text">
+        <span style={{ fontFamily: 'Inter, sans-serif', fontWeight: 500 }}>
+          Organized by ion category
+        </span>
+      </div>
     </div>
   );
 }
